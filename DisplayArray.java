f084a1ea6e4f66344929b5e4691fcb3a66bbc73d
Gene Wagner <wgnr.gn.s@gmail.com>
2021-08-29 13:16:58 -0700

    first commit

---
+++ DisplayArray.java
@@ -0,0 +1,20 @@
+public class DisplayArray
+{
+   public static void main(String[] args)
+   {
+      int[] a1 = {1,3,5,7,9};
+      printArray(a1, 0);
+      int[] a2 = {100};
+      printArray(a2, 0);
+   }
+   public static void printArray(int[] a, int index)
+   {
+      if (index < a.length)
+      {
+         System.out.printf("%d ", a[index]);
+         printArray(a, index + 1);
+      }
+      else
+         System.out.println();
+   }
+}
